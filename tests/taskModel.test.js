7d66f2d546a18d550bbaa32dda6486071252b473
Dor Shalom <dorshalom96@gmail.com>
2026-01-22 19:15:40 +0200

    Remove tests

--- tests/taskModel.test.js
+++
@@ -1,9 +0,0 @@
-import { createTask, isValidTask } from '../src/models/taskModel.js';
-
-test('createTask returns a valid task', () => {
-  const t = createTask('hello');
-  expect(typeof t.id).toBe('string');
-  expect(t.text).toBe('hello');
-  expect(t.completed).toBe(false);
-  expect(isValidTask(t)).toBe(true);
-});
